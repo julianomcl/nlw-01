e8d32c166b3a94e0c8851649e96d5ed17afca8a4
Juliano Maia <leite.juliano@gmail.com>
2020-06-07 00:20:01 -0300

    App

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+  async index(req: Request, res: Response) {
+    try {
+      const items = await knex('items').select('*');
+  
+      const serializedItems = items.map(item => {
+        return { 
+          id: item.id,
+          title: item.title,
+          image_url: `http://192.168.0.12:3333/uploads/${item.image}`,
+        };
+      });
+  
+      return res.json(serializedItems);
+    } catch (e) {
+      console.error(e);
+      return res.status(500);
+    }
+  }
+};
+
+export default ItemsController;
